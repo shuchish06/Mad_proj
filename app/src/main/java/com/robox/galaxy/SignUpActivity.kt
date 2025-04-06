b48fbd212b24e3d53bfd77fbf1fb0f803ca7ca7e
Shuchi <shuchi0709@gmail.com>
2025-04-06 18:27:55 +0530

    login and signup page

---
+++ app/src/main/java/com/robox/galaxy/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.robox.galaxy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup)
+    }
+}
