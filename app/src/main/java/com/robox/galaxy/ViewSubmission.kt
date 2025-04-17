52e2b2cc796a1af65042c8d7d794a8dc83093209
Radhika Agrawal <radhikaagrawal1905@gmail.com>
2025-04-17 22:53:07 +0530

    View Submission linked

---
+++ app/src/main/java/com/robox/galaxy/ViewSubmission.kt
@@ -0,0 +1,16 @@
+package com.robox.galaxy
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ViewSubmission : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_view_submission)
+
+    }
+}
